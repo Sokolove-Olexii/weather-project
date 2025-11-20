8add04f053591ace33d0efec1f9d1ec77cba3a64
Sokolove-Olexii <130235909+Sokolove-Olexii@users.noreply.github.com>
2025-11-21 01:54:50 +0200

    MUI modal rewrite

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -13,59 +13,173 @@ export default function SignInModal({ open, onClose }) {
     e.preventDefault();
     const email = e.target.email.value;
     const password = e.target.password.value;
-
     console.log("Email:", email);
     console.log("Password:", password);
-
     onClose();
   };
 
   return (
     <Dialog
       open={open}
-      onClose={onclose}
+      onClose={onClose}
       PaperProps={{
         sx: {
-          borderRadius: 3.5, // 4 = 32px
+          borderRadius: "25px",
+          padding: "28px 80px",
+          width: "600px",
+          textAlign: "center",
         },
       }}
     >
-      <DialogTitle>Sign In</DialogTitle>
+      <DialogTitle
+        sx={{
+          fontFamily: "Montserrat Alternates",
+          fontSize: "28px",
+          fontWeight: 500,
+          lineHeight: "34px",
+          mb: 3,
+          textAlign: "center",
+        }}
+      >
+        Sign up
+      </DialogTitle>
+
       <DialogContent>
         <Box
-          components="form"
+          component="form"
           onSubmit={handleSubmit}
-          sx={{ display: "flex", flexDirection: "column", gap: 2, mt: 1 }}
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            gap: 3,
+          }}
         >
-          <TextField
-            name="username"
-            label="Username"
-            type="username"
-            required
-            fullWidth
-          />
-          <TextField
-            name="email"
-            label="Email"
-            type="email"
-            required
-            fullWidth
-          />
-          <TextField
-            name="password"
-            label="Password"
-            type="password"
-            required
-            fullWidth
-          />
+          <Box sx={{ width: "100%", textAlign: "left" }}>
+            <Typography
+              sx={{
+                fontSize: "14px",
+                fontWeight: 500,
+                lineHeight: "17px",
+                marginBottom: "15px",
+              }}
+            >
+              Username
+            </Typography>
+            <TextField
+              name="username"
+              placeholder="Username"
+              fullWidth
+              sx={{
+                color: "rgba(171, 171, 171, 1)",
+                fontFamily: "Montserrat",
+                fontSize: "14px",
+                fontWeight: 500,
+                lineHeight: "17px",
+                background: "rgba(228, 228, 228, 1)",
+                borderRadius: "10px",
+                "& fieldset": { border: "none" },
+              }}
+            />
+          </Box>
 
-          <Button type="submit" variant="contained" fullWidth>
+          <Box sx={{ width: "100%", textAlign: "left" }}>
+            <Typography
+              sx={{
+                fontSize: "14px",
+                fontWeight: 500,
+                lineHeight: "17px",
+                marginBottom: "15px",
+              }}
+            >
+              E-Mail
+            </Typography>
+            <TextField
+              name="email"
+              placeholder="E-Mail"
+              fullWidth
+              sx={{
+                color: "rgba(171, 171, 171, 1)",
+                fontFamily: "Montserrat",
+                fontSize: "14px",
+                fontWeight: 500,
+                lineHeight: "17px",
+                background: "rgba(228, 228, 228, 1)",
+                borderRadius: "10px",
+                "& fieldset": { border: "none" },
+              }}
+            />
+          </Box>
+
+          <Box sx={{ width: "100%", textAlign: "left" }}>
+            <Typography
+              sx={{
+                fontSize: "14px",
+                fontWeight: 500,
+                lineHeight: "17px",
+                marginBottom: "15px",
+              }}
+            >
+              Password
+            </Typography>
+            <TextField
+              name="password"
+              placeholder="Password"
+              type="password"
+              fullWidth
+              sx={{
+                color: "rgba(171, 171, 171, 1)",
+                fontFamily: "Montserrat",
+                fontSize: "14px",
+                fontWeight: 500,
+                lineHeight: "17px",
+                background: "rgba(228, 228, 228, 1)",
+                borderRadius: "10px",
+                "& fieldset": { border: "none" },
+              }}
+            />
+          </Box>
+
+          <Button
+            type="submit"
+            variant="contained"
+            sx={{
+              width: "114px",
+              height: "37px",
+              padding: "10px 30px 10px 30px",
+              borderRadius: "10px",
+              backgroundColor: "rgba(255, 179, 108, 1)",
+              color: "black",
+              fontFamily: "Montserrat",
+              textTransform: "none",
+              fontSize: "14px",
+              fontWeight: "400",
+              lineHeight: "17px",
+              mt: 2,
+            }}
+          >
             Sign Up
           </Button>
         </Box>
 
-        <Typography sx={{ mt: 2, textAlign: "center", fontSize: 14 }}>
-          Already have an account? <strong>Log in</strong>
+        <Typography
+          sx={{
+            color: "rgba(0, 0, 0, 1)",
+            fontFamily: "Montserrat",
+            fontSize: "10px",
+            fontWeight: 500,
+            lineHeight: "12px",
+          }}
+        >
+          Already have an account?{" "}
+          <span
+            style={{
+              textDecoration: "underline",
+              cursor: "pointer",
+            }}
+          >
+            Log In
+          </span>
         </Typography>
       </DialogContent>
     </Dialog>
