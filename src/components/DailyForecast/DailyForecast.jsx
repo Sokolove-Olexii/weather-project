8f034f1688f4aae8882fb37ab0fa95c9633361ac
Sokolove-Olexii <130235909+Sokolove-Olexii@users.noreply.github.com>
2025-12-04 01:47:10 +0200

    added styles
    some adaptive for SeeMore and DailyForecast section

--- src/components/DailyForecast/DailyForecast.jsx
+++ src/components/DailyForecast/DailyForecast.jsx
@@ -1,48 +1,61 @@
 import styles from "./DailyForecast.module.scss";
+import { Container } from "../Container/Container";
 
 export const DailyForecast = ({ forecast }) => {
   if (!forecast) return null;
 
   return (
-    <div className={styles.DailyForecast}>
-      <p className={styles.DailyForecast_title}>8-day forecast</p>
-      <ul className={styles.DailyForecastList}>
-        {forecast.map((day, index) => {
-          const date = new Date(day.dt * 1000);
-          const dateString = date.toLocaleDateString("en-GB", {
-            weekday: "short",
-            month: "short",
-            day: "numeric",
-          });
-          return (
-            <li key={day.dt} className={styles.DailyForecastList_li}>
-              <span className={styles.DailyForecastList_date}>
-                {dateString}
-              </span>
+    <div className={styles.overlay}>
+      <Container>
+        <div className={styles.DailyForecast}>
+          <p className={styles.DailyForecast_title}>8-day forecast</p>
+          <ul className={styles.DailyForecastList}>
+            {forecast.map((day, index) => {
+              const date = new Date(day.dt * 1000);
+              const dateString = date.toLocaleDateString("en-GB", {
+                weekday: "short",
+                month: "short",
+                day: "numeric",
+              });
+              return (
+                <li key={day.dt} className={styles.DailyForecastList_li}>
+                  <span className={styles.DailyForecastList_date}>
+                    {dateString}
+                  </span>
 
-              {/* <p className={styles.condition}>{day.weather[0].description}</p> */}
+                  {/* <p className={styles.condition}>{day.weather[0].description}</p> */}
 
-              <div className={styles.DailyForecastListTemperature}>
-                <img
-                  src={`http://openweathermap.org/img/wn/${day.weather[0].icon}.png`}
-                  alt={day.weather[0].description}
-                  className={styles.DailyForecastList_icon}
-                />
-                <span className={styles.DailyForecastListTemperature_text}>
-                  {Math.round(day.temp.day)}°C/
-                </span>
-                <span className={styles.DailyForecastListTemperature_text}>
-                  {Math.round(day.temp.night)}°C
-                </span>
-              </div>
+                  <div className={styles.DailyForecastListTemperature}>
+                    <img
+                      src={`http://openweathermap.org/img/wn/${day.weather[0].icon}.png`}
+                      alt={day.weather[0].description}
+                      className={styles.DailyForecastList_icon}
+                    />
+                    <div
+                      className={styles.DailyForecastListTemperature_position}
+                    >
+                      <span
+                        className={styles.DailyForecastListTemperature_text}
+                      >
+                        {Math.round(day.temp.day)}°C/
+                      </span>
+                      <span
+                        className={styles.DailyForecastListTemperature_text}
+                      >
+                        {Math.round(day.temp.night)}°C
+                      </span>
+                    </div>
+                  </div>
 
-              <span className={styles.DailyForecastList_description}>
-                {day.weather[0].description}
-              </span>
-            </li>
-          );
-        })}
-      </ul>
+                  <span className={styles.DailyForecastList_description}>
+                    {day.weather[0].description}
+                  </span>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </Container>
     </div>
   );
 };
--- src/components/SeeMore/SeeMore.jsx
+++ src/components/SeeMore/SeeMore.jsx
@@ -1,5 +1,5 @@
 import styles from "./SeeMore.module.scss";
-import Container from "../Container/Container";
+import { Container } from "../Container/Container";
 import temperature from "../../images/SeeMore/temperature.svg";
 import humidity from "../../images/SeeMore/humidity.svg";
 import pressure from "../../images/SeeMore/pressure.svg";
@@ -10,63 +10,68 @@ import { DailyForecast } from "../DailyForecast/DailyForecast";
 export const SeeMore = ({ data, onClose, forecast }) => {
   return (
     <div className={styles.overlay} onClick={onClose}>
-      <section className={styles.SeeMore} onClick={(e) => e.stopPropagation()}>
-        {/* <Container> */}
-        <ul className={styles.SeeMoreList}>
-          <li className={styles.SeeMoreList_li}>
-            <p className={styles.SeeMoreList_text}>Feels like</p>
-            <p className={styles.SeeMoreList_data}>{data.main.feels_like} °C</p>
-            <img
-              src={temperature}
-              alt="temperatureIcon"
-              className={styles.SeeMoreList_icons}
-            />
-          </li>
-          <li className={styles.SeeMoreList_li}>
-            <p className={styles.SeeMoreList_text}>Min °C</p>
-            <p className={styles.SeeMoreList_data}>{data.main.temp_min}°C</p>
-            <p className={styles.SeeMoreList_text}>Max °C</p>
-            <p className={styles.SeeMoreList_data}>{data.main.temp_max}°C</p>
-          </li>
-          <li className={styles.SeeMoreList_li}>
-            <p className={styles.SeeMoreList_text}>Humidity</p>
-            <p className={styles.SeeMoreList_data}>{data.main.humidity}%</p>
-            <img
-              src={humidity}
-              alt="humidityIcon"
-              className={styles.SeeMoreList_icons}
-            />
-          </li>
-          <li className={styles.SeeMoreList_li}>
-            <p className={styles.SeeMoreList_text}>Pressure</p>
-            <p className={styles.SeeMoreList_data}>{data.main.pressure} Pa</p>
-            <img
-              src={pressure}
-              alt="pressureIcon"
-              className={styles.SeeMoreList_icons}
-            />
-          </li>
-          <li className={styles.SeeMoreList_li}>
-            <p className={styles.SeeMoreList_text}>Wind speed</p>
-            <p className={styles.SeeMoreList_data}>{data.wind.speed} m/s</p>
-            <img
-              src={windSpeed}
-              alt="windSpeedIcon"
-              className={styles.SeeMoreList_icons}
-            />
-          </li>
-          <li className={styles.SeeMoreList_li}>
-            <p className={styles.SeeMoreList_text}>Visibility</p>
-            <p className={styles.SeeMoreList_data}>{data.visibility + "m"}</p>
-            <img
-              src={visibility}
-              alt="visibilityIcon"
-              className={styles.SeeMoreList_icons}
-            />
-          </li>
-        </ul>
-        {/* </Container> */}
-      </section>
+      <Container>
+        <section
+          className={styles.SeeMore}
+          onClick={(e) => e.stopPropagation()}
+        >
+          <ul className={styles.SeeMoreList}>
+            <li className={styles.SeeMoreList_li}>
+              <p className={styles.SeeMoreList_text}>Feels like</p>
+              <p className={styles.SeeMoreList_data}>
+                {data.main.feels_like} °C
+              </p>
+              <img
+                src={temperature}
+                alt="temperatureIcon"
+                className={styles.SeeMoreList_icons}
+              />
+            </li>
+            <li className={styles.SeeMoreList_li}>
+              <p className={styles.SeeMoreList_text}>Min °C</p>
+              <p className={styles.SeeMoreList_data}>{data.main.temp_min}°C</p>
+              <p className={styles.SeeMoreList_text}>Max °C</p>
+              <p className={styles.SeeMoreList_data}>{data.main.temp_max}°C</p>
+            </li>
+            <li className={styles.SeeMoreList_li}>
+              <p className={styles.SeeMoreList_text}>Humidity</p>
+              <p className={styles.SeeMoreList_data}>{data.main.humidity}%</p>
+              <img
+                src={humidity}
+                alt="humidityIcon"
+                className={styles.SeeMoreList_icons}
+              />
+            </li>
+            <li className={styles.SeeMoreList_li}>
+              <p className={styles.SeeMoreList_text}>Pressure</p>
+              <p className={styles.SeeMoreList_data}>{data.main.pressure} Pa</p>
+              <img
+                src={pressure}
+                alt="pressureIcon"
+                className={styles.SeeMoreList_icons}
+              />
+            </li>
+            <li className={styles.SeeMoreList_li}>
+              <p className={styles.SeeMoreList_text}>Wind speed</p>
+              <p className={styles.SeeMoreList_data}>{data.wind.speed} m/s</p>
+              <img
+                src={windSpeed}
+                alt="windSpeedIcon"
+                className={styles.SeeMoreList_icons}
+              />
+            </li>
+            <li className={styles.SeeMoreList_li}>
+              <p className={styles.SeeMoreList_text}>Visibility</p>
+              <p className={styles.SeeMoreList_data}>{data.visibility + "m"}</p>
+              <img
+                src={visibility}
+                alt="visibilityIcon"
+                className={styles.SeeMoreList_icons}
+              />
+            </li>
+          </ul>
+        </section>
+      </Container>
     </div>
   );
 };
